61eff7f63670b054cc76bfb804b7428f9e24cc5c
keita1899 <saboten18k@gmail.com>
2026-01-17 00:30:58 +0900

    refactor: ダイアログコンポーネントをFormDialogに統一
    - FormDialogコンポーネントを作成して共通化
    - EventDialog, TaskDialog, YearlyGoalDialog, MonthlyGoalDialogをFormDialogを使用するように変更
    - DialogDescriptionを削除
    - コードの重複を削減（各ダイアログが約20行→約10行）

--- components/goals/MonthlyGoalDialog.tsx
+++ components/goals/MonthlyGoalDialog.tsx
@@ -1,12 +1,6 @@
 'use client'
 
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from '@/components/ui/dialog'
+import { FormDialog } from '@/components/ui/form-dialog'
 import { MonthlyGoalForm } from './MonthlyGoalForm'
 import type { MonthlyGoal, CreateMonthlyGoalInput } from '@/lib/types/monthly-goal'
 
@@ -25,34 +19,23 @@ export const MonthlyGoalDialog = ({
   goal,
   selectedYear,
 }: MonthlyGoalDialogProps) => {
-  const handleSubmit = async (input: CreateMonthlyGoalInput) => {
-    await onSubmit(input)
-    onOpenChange(false)
-  }
-
-  const isEditMode = !!goal
-
   return (
-    <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>
-            {isEditMode ? '月間目標を編集' : '新しい月間目標を作成'}
-          </DialogTitle>
-          <DialogDescription>
-            {isEditMode
-              ? '月間目標の詳細を編集してください'
-              : '月間目標の詳細を入力してください'}
-          </DialogDescription>
-        </DialogHeader>
-        <MonthlyGoalForm
-          onSubmit={handleSubmit}
-          onCancel={() => onOpenChange(false)}
-          initialData={goal}
-          submitLabel={isEditMode ? '更新' : '作成'}
-          selectedYear={selectedYear}
-        />
-      </DialogContent>
-    </Dialog>
+    <FormDialog<
+      CreateMonthlyGoalInput,
+      MonthlyGoal,
+      { selectedYear?: number }
+    >
+      open={open}
+      onOpenChange={onOpenChange}
+      onSubmit={onSubmit}
+      initialData={goal}
+      title={{
+        create: '新しい月間目標を作成',
+        edit: '月間目標を編集',
+      }}
+      formComponent={MonthlyGoalForm}
+      formProps={{ selectedYear }}
+      closeOnSubmit={true}
+    />
   )
 }
--- components/goals/YearlyGoalDialog.tsx
+++ components/goals/YearlyGoalDialog.tsx
@@ -1,12 +1,6 @@
 'use client'
 
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from '@/components/ui/dialog'
+import { FormDialog } from '@/components/ui/form-dialog'
 import { YearlyGoalForm } from './YearlyGoalForm'
 import type { YearlyGoal, CreateYearlyGoalInput } from '@/lib/types/yearly-goal'
 
@@ -25,34 +19,23 @@ export const YearlyGoalDialog = ({
   goal,
   selectedYear,
 }: YearlyGoalDialogProps) => {
-  const handleSubmit = async (input: CreateYearlyGoalInput) => {
-    await onSubmit(input)
-    onOpenChange(false)
-  }
-
-  const isEditMode = !!goal
-
   return (
-    <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>
-            {isEditMode ? '年間目標を編集' : '新しい年間目標を作成'}
-          </DialogTitle>
-          <DialogDescription>
-            {isEditMode
-              ? '年間目標の詳細を編集してください'
-              : '年間目標の詳細を入力してください'}
-          </DialogDescription>
-        </DialogHeader>
-        <YearlyGoalForm
-          onSubmit={handleSubmit}
-          onCancel={() => onOpenChange(false)}
-          initialData={goal}
-          submitLabel={isEditMode ? '更新' : '作成'}
-          selectedYear={selectedYear}
-        />
-      </DialogContent>
-    </Dialog>
+    <FormDialog<
+      CreateYearlyGoalInput,
+      YearlyGoal,
+      { selectedYear?: number }
+    >
+      open={open}
+      onOpenChange={onOpenChange}
+      onSubmit={onSubmit}
+      initialData={goal}
+      title={{
+        create: '新しい年間目標を作成',
+        edit: '年間目標を編集',
+      }}
+      formComponent={YearlyGoalForm}
+      formProps={{ selectedYear }}
+      closeOnSubmit={true}
+    />
   )
 }
