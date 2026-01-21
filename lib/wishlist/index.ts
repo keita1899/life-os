06969774dcc246a5408e7a75e234e4d3d95d44ce
keita1899 <saboten18k@gmail.com>
2026-01-21 23:26:56 +0900

    chore: 不要なlib/wishlistディレクトリを削除

--- lib/wishlist/index.ts
+++
@@ -1,7 +0,0 @@
-export {
-  getAllBucketListCategories,
-  createBucketListCategory,
-  updateBucketListCategory,
-  deleteBucketListCategory,
-} from './category'
-export * from './bucket-list-item'
